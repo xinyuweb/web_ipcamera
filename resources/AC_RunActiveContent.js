a6d59832e3647f55261e40b425c8f107fbaab9e3
jweihsz <1989428128@qq.com>
2016-08-31 17:41:06 +0800

    20160831_2

--- resources/AC_RunActiveContent.js
+++ resources/AC_RunActiveContent.js
@@ -223,13 +223,11 @@ function AC_Generateobj(objAttrs, params, embedAttrs)
   document.write(str);
 }
 
-function AC_FL_RunContent(){
-  var ret = 
-    AC_GetArgs
-    (  arguments, ".swf", "movie", "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
-     , "application/x-shockwave-flash"
-    );
+function AC_FL_RunContent()
+{
+  var ret = AC_GetArgs(arguments, ".swf", "movie", "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000", "application/x-shockwave-flash");
   AC_Generateobj(ret.objAttrs, ret.params, ret.embedAttrs);
+
 }
 
 function AC_SW_RunContent(){
